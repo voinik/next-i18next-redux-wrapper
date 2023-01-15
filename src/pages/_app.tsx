d4e352fc6cf756cefda7de792596db1756dad71d
voinik <victor_panteleev@hotmail.com>
2023-01-15 19:45:13 +0100

    Setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,46 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import { Provider } from "react-redux";
+import App, { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+import { fetchSystem, wrapper } from "../store";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface PageProps {
+  pageProps: {
+    id: number;
+  };
 }
+
+const MyApp = ({
+  Component,
+  ...rest
+}: Omit<AppProps, "pageProps"> & PageProps) => {
+  console.log("rest: ", rest);
+  const { store, props } = wrapper.useWrappedStore(rest);
+
+  return (
+    <Provider store={store}>
+      <h1>PageProps.id: {rest.pageProps.id}</h1>
+      <Component {...props.pageProps} />
+    </Provider>
+  );
+};
+
+MyApp.getInitialProps = wrapper.getInitialAppProps(
+  (store) =>
+    async (appCtx): Promise<PageProps> => {
+      // You have to do dispatches first, before...
+      await store.dispatch(fetchSystem());
+
+      // ...before calling (and awaiting!!!!) the children's getInitialProps
+      const childrenGip = await App.getInitialProps(appCtx);
+      return {
+        pageProps: {
+          // And you have to spread the children's GIP result into pageProps
+          ...childrenGip.pageProps,
+          id: 42,
+        },
+      };
+    }
+);
+
+export default appWithTranslation(MyApp);
